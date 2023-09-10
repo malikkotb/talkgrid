fce8b0944ebc371bcf7fefd673b5c26e950c3972
Malik Kotb <malikkotb@icloud.com>
2023-09-10 17:44:16 +0200

    delete dialog and button

---
+++ app/dashboard/DeleteDialog.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function DeleteDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Delete</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Are you sure you want to delete this post?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            post.
+          </DialogDescription>
+        </DialogHeader>
+        {/* <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div> */}
+        <DialogFooter>
+          <Button type="submit" variant={"destructive"}>Delete Post</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useState } from "react";
 import {
@@ -11,6 +11,7 @@ import {
   CardTitle,
 } from "../../components/ui/card";
 import Link from "next/link";
+import DeleteDialog from "./DeleteDialog";
 
 type EditProps = {
   id: string;
@@ -34,31 +35,36 @@ export default function EditPost({
   id,
 }: EditProps) {
   return (
-    <Card>
-      <CardHeader className="flex flex-row items-center gap-2">
-        <Image
-          className="w-10 rounded-full"
-          width={64}
-          height={64}
-          alt="user image"
-          src={avatar}
-          priority
-        ></Image>
-        <CardTitle>{name}</CardTitle>
-      </CardHeader>
-      <CardContent className="grid gap-6">
-        <div className="grid gap-2">
-          <p className="line-clamp-3">{title}</p>
-        </div>
-      </CardContent>
-      <CardFooter className="flex justify-between space-x-2">
-        <Link href={`/post/${id}`}>
-          <p className="text-sm cursor-pointer font-bold">
-            {comments?.length} Comments
-          </p>
-        </Link>
-        <Button variant="ghost" className="text-red-700 font-bold">Delete</Button>
-      </CardFooter>
-    </Card>
+    <>
+      <Card>
+        <CardHeader className="flex flex-row items-center gap-2">
+          <Image
+            className="w-10 rounded-full"
+            width={64}
+            height={64}
+            alt="user image"
+            src={avatar}
+            priority
+          ></Image>
+          <CardTitle>{name}</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-6">
+          <div className="grid gap-2">
+            <p className="line-clamp-3">{title}</p>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between space-x-2">
+          <Link href={`/post/${id}`}>
+            <p className="text-sm cursor-pointer font-bold">
+              {comments?.length} Comments
+            </p>
+          </Link>
+          <Button variant="link" className="text-red-700 hover:no-underline font-bold">
+            
+            <DeleteDialog />
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
   );
 }
