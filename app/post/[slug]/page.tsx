a3cda3ac2819ad84c2d54b899e68699918a21f78
Malik Kotb <malikkotb@icloud.com>
2023-09-10 18:32:47 +0200

    individual post page

---
+++ app/post/[slug]/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import axios from "axios";
+import Post from "../../../components/Post";
+import { useQuery } from "@tanstack/react-query";
+import { PostType } from "@/app/types/Posts";
+
+type URL = {
+  params: {
+    slug: string;
+  };
+};
+
+const fetchDetails = async (slug: string) => {
+  const response = await axios.get(`api/posts/${slug}`);
+  return response.data;
+};
+
+export default function PostDetail(url: URL) {
+  const { data, isLoading } = useQuery<PostType[]>({
+    queryKey: ["detail-post"],
+    queryFn: () => fetchDetails(url.params.slug), // get id of post through url.params.slug
+  });
+
+  if (isLoading) return <div className="flex text-lg font-bold justify-center">Loading...</div>
+  console.log(data);
+
+  return <div>Hi</div>;
+}
